7c1a9df7d3d5d26aa4036a5961eaa536f2134545
Luispiovan <luisguilherme.piovan@hotmail.com>
2020-12-15 16:42:10 -0300

    criação de componentes cadastro-filmes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { LayoutModule } from "@angular/cdk/layout";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MAT_DATE_LOCALE } from "@angular/material/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app.routing.module";
+
+import { MaterialModule } from "./shared/material/material.module";
+import { TopoComponent } from "./shared/components/topo/topo.component";
+import { RodapeComponent } from "./shared/components/rodape/rodape.component";
+import { AlertaComponent } from "./shared/components/alerta/alerta.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, TopoComponent, RodapeComponent, AlertaComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    LayoutModule,
+    MaterialModule,
+    AppRoutingModule
+  ],
+  entryComponents: [AlertaComponent],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: "pt" }],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
